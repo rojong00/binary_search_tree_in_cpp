7f8fda316522c6033990b9a6a8796efb7d03fa83
rojong00 <49092381+rojong00@users.noreply.github.com>
2024-02-28 22:23:11 +0900

    amend bst.cpp (deleting unnecessary var) and add execution example img

--- bst.cpp
+++ bst.cpp
@@ -19,7 +19,6 @@ void BSTtree::displayNode2(Node* _Node){
 }
 
 void BSTtree::addNode(int _value){
-	int tmptmp = 123;
 	Node* newNode = new Node;
 	newNode->value = _value;
 	newNode->parent = nullptr;
@@ -188,15 +187,6 @@ Node* BSTtree::search(int _value){
     if (chkExit == 2)
         return nullptr;
     return tmp;
-}
-
-void BSTtree::getTreeGraph()
-{
-	int Graph = 0;
-	while (true)
-		{
-			break;
-		}
 }
 
 void BSTtree::searchNode(int value_)
